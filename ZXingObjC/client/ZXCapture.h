97a7fe9152b1f87f9c52500e3394b198274c4cc7
bvhieubstar <hieu.bui@bstarsolutions.com>
2020-01-07 14:34:02 +0700

    -Update IphonX +

--- ZXingObjC/client/ZXCapture.h
+++ ZXingObjC/client/ZXCapture.h
@@ -18,12 +18,8 @@
 
 @protocol ZXCaptureDelegate, ZXReader;
 @class ZXDecodeHints;
-
-@interface ZXCapture : NSObject <AVCaptureVideoDataOutputSampleBufferDelegate, CAAction
-#if defined(__MAC_10_12) && __MAC_OS_X_VERSION_MAX_ALLOWED >= __MAC_10_12 || defined(__IPHONE_10_0) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_10_0
-, CALayerDelegate
-#endif
->
+//Update for IPHONE X+
+@interface ZXCapture : NSObject <AVCaptureVideoDataOutputSampleBufferDelegate, CAAction, CALayerDelegate>
 
 @property (nonatomic, assign) int camera;
 @property (nonatomic, strong) AVCaptureDevice *captureDevice;
